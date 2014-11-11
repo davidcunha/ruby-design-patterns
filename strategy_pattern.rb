8407a3fb4e8ab98ae9f834ef7218efd7bd280495
davidcunha <davidgoncalvescunha@gmail.com>
2014-11-11 22:00:01 +0000

    Refactor strategy pattern - concrete classes and lambda

--- strategy_pattern.rb
+++ strategy_pattern.rb
@@ -1,21 +1,77 @@
-#Strategy Pattern
-
+# Strategy Pattern
 class Report
+  attr_reader :title, :text
+  attr_accessor :formatter
 
   def initialize(formatter)
     @title = 'Monthly Report'
-    @text = ['Things are going', 'really, really well.']
+    @text =  ['Things are going', 'really, really well.']
     @formatter = formatter
   end
 
   def output_report
-    @formatter.call(self)
+    @formatter.output_report(self)
+  end
+end
+
+class HTMLFormatter
+  def output_report(context)
+    puts('<html>')
+    puts('  <head>')
+    # Output The rest of the report ...
+    puts("    <title>#{context.title}</title>")
+    puts('  </head>')
+    puts('  <body>')
+    context.text.each do |line|
+      puts("    <p>#{line}</p>")
+    end
+    puts('  </body>')
+    puts('</html>')
+  end
+end
+
+class PlainTextFormatter
+  def output_report(context)
+    puts("***** #{context.title} *****")
+    context.text.each do |line|
+      puts(line)
+    end
+  end
+end
+
+report = Report.new HTMLFormatter.new
+report.output_report
+
+
+# Strategy Pattern with lamdba
+class Report
+  attr_reader :title, :text
+  attr_accessor :formatter
+
+  def initialize(&formatter)
+    @title = 'Monthly Report'
+    @text = [ 'Things are going', 'really, really well.' ]
+    @formatter = formatter
   end
 
+  def output_report
+    @formatter.call( self )
+  end
 end
 
 HTML_FORMATTER = lambda do |context|
-... code to output HTML
+  puts('<html>')
+  puts('  <head>')
+  # Output The rest of the report ...
+  puts("    <title>#{context.title}</title>")
+  puts('  </head>')
+  puts('  <body>')
+  context.text.each do |line|
+    puts("    <p>#{line}</p>")
+  end
+  puts('  </body>')
+  puts('</html>')
+end
 
-report = Report.new(HTML_FORMATTER)
+report = Report.new &HTML_FORMATTER
 report.output_report
