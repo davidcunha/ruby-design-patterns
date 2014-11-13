fd4585fcc378bb97ff546e061e92b1b4c79132d2
davidcunha <davidgoncalvescunha@gmail.com>
2014-11-13 00:34:21 +0000

    Refactor Observer Pattern

--- observer_pattern.rb
+++ observer_pattern.rb
@@ -1,11 +1,31 @@
-#Observer Pattern
+# Observer Pattern
+module Subject
+  def initialize
+    @observers=[]
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+end
 
 class Employee
-  include Observable
+  include Subject
+  attr_reader :name, :address
+  attr_reader :salary
 
-  @attr_reader :name, :title, :salary
-
-  def initialize(name,title,salary)
+  def initialize( name, title, salary)
+    super()
     @name = name
     @title = title
     @salary = salary
@@ -13,8 +33,22 @@ class Employee
 
   def salary=(new_salary)
     @salary = new_salary
-    changed
-    notify_observers(self)
+    notify_observers
+  end
+end
+
+class Payroll
+  def initialize
   end
 
+  def update(subject)
+    puts "#{subject.name} salary is now #{subject.salary}!"
+  end
 end
+
+fred = Employee.new('Fred', 'Crane Operator', 30000.0)
+
+payroll = Payroll.new
+fred.add_observer(payroll)
+
+fred.salary = 35000.0
