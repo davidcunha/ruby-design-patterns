88d0a38ff48aae32e8e8cea6fb783a52e5591b92
davidcunha <davidgoncalvescunha@gmail.com>
2014-11-24 01:10:31 +0000

    Composite Pattern

---
+++ composite_pattern.rb
@@ -0,0 +1,67 @@
+# Composite Pattern
+class Task
+  attr_accessor :name, :parent
+
+  def initialize(name)
+    @name = name
+    @parent = nil
+  end
+
+  def get_time_required
+    0.0
+  end
+end
+
+class CompositeTask < Task
+  def initialize(name)
+    super(name)
+    @sub_tasks = []
+  end
+
+  def add_sub_task(task)
+    @sub_tasks << task
+    task.parent = self
+  end
+
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+    task.parent = nil
+  end
+
+  def get_time_required
+    time = 0.0
+    @sub_tasks.each { |task| time += task.get_time_required }
+    time
+  end
+end
+
+class MakeBatterTask < CompositeTask
+  def initialize
+    super('Make batter')
+    add_sub_task( AddDryIngredientsTask.new )
+    add_sub_task( MixTask.new )
+  end
+end
+
+class AddDryIngredientsTask < Task
+  def initialize
+    super('Add dry ingredients')
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
+class MixTask < Task
+  def initialize
+    super('Mix that batter up!')
+  end
+
+  def get_time_required
+    3.0
+  end
+end
+
+composite_task = MakeBatterTask.new()
+puts composite_task.get_time_required
