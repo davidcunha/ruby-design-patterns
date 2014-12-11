14d40f747e22ca86097948530bd34f176884a08c
davidcunha <davidgoncalvescunha@gmail.com>
2014-12-11 10:33:56 +0000

    Adapter pattern

--- adapter_pattern.rb
+++ adapter_pattern.rb
@@ -1,16 +1,73 @@
-#Adapter Pattern
-
-require 'lostechies_text_object' #original class
-
-#add some methods to original class
-class LosTechiesTextObject
-
-  def sponsor
-    return friend_of_pablo
+# Adapter Pattern
+class Encrypter
+  def initialize(key)
+    @key = key
   end
 
-  def blogger
-    return follower_of_pablo
+  def encrypt(reader, writer)
+    key_index = 0
+    while not reader.eof?
+      clear_char = reader.getc
+      encrypted_char = clear_char ^ @key[key_index]
+      writer.putc(encrypted_char)
+      key_index = (key_index + 1) % @key.size
+    end
+  end
+end
+
+class StringIOAdapter
+  def initialize(string)
+    @string = string
+    @position = 0
   end
 
+  def getc
+    if @position >= @string.length
+      raise EOFError
+    end
+    ch = @string[@position]
+    @position += 1
+    return ch
+  end
+
+  def eof?
+    return @position >= @string.length
+  end
+end
+
+reader = File.open('message.txt')
+reader = StringIOAdapter.new('We attack at dawn')
+encrypter = Encrypter.new('my secret key')
+encrypter.encrypt(reader, writer)
+
+# Adapter Pattern with monkey patching
+class TextObject
+  attr_reader :text, :size_inches, :color
+  def initialize(text, size_inches, color)
+    @text = text
+    @size_inches = size_inches
+    @color = color
+  end
+end
+
+class BritishTextObject
+  attr_reader :string, :size_mm, :colour
+end
+
+# Make sure the original class is loaded
+require 'british_text_object'
+
+# Now add some methods to the original class
+class BritishTextObject
+  def color
+    return colour
+  end
+
+  def text
+    return string
+  end
+
+  def size_inches
+    return size_mm / 25.4
+  end
 end
