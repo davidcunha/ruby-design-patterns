6728f387c2fe57ff5559ac6ae8eba0a2e5264684
davidcunha <davidgoncalvescunha@gmail.com>
2014-12-20 20:53:29 +0000

    Proxy pattern

--- proxy_pattern.rb
+++ proxy_pattern.rb
@@ -1,19 +1,91 @@
-#Proxy Pattern
+class BankAccount
+  attr_reader :balance
+
+  def initialize(starting_balance = 0)
+    @balance = starting_balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+end
+
+# Proxy pattern (protection proxy)
+require 'etc'
+
+class AccountProtectionProxy
+  def initialize(real_account, owner_name)
+    @subject = real_account
+    @owner_name = owner_name
+  end
+
+  def deposit(amount)
+    check_access
+    return @subject.deposit(amount)
+  end
+
+  def withdraw(amount)
+    check_access
+    return @subject.withdraw(amount)
+  end
+
+  def balance
+    check_access
+    return @subject.balance
+  end
+
+  def check_access
+    if Etc.getlogin != @owner_name
+      raise "Illegal access: #{Etc.getlogin} cannot access account."
+    end
+  end
+end
+
+# Proxy pattern (virtual proxy)
+
+class VirtualAccountProxy
+  def initialize(starting_balance=0)
+    @starting_balance = starting_balance
+  end
+
+  def deposit(amount)
+    s = subject
+    return s.deposit(amount)
+  end
+
+  def withdraw(amount)
+    s = subject
+    return s.withdraw(amount)
+  end
+
+  def balance
+    s = subject
+    return s.balance
+  end
+
+  def subject
+    @subject || (@subject = BankAccount.new(@starting_balance))
+  end
+end
+
+# Proxy pattern (method_missing)
 
 class AccountProxy
-
   def initialize(real_account)
     @subject = real_account
   end
 
-  def method_missing(name,*args)
-    puts "delegating #{name} message to subject."
-    @subject.send(name,*args)
+  def method_missing(name, *args)
+    puts("Delegating #{name} message to subject.")
+    @subject.send(name, *args)
   end
-
 end
 
-ap = AccountProxy.new(BankAccount.new(100))
+ap = AccountProxy.new( BankAccount.new(100) )
 ap.deposit(25)
 ap.withdraw(50)
-puts "account balance is now: #{ap.balance}"
+puts("account balance is now: #{ap.balance}")
