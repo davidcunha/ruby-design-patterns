128259a39b5fbf94b2838573f2baf0a685c93116
davidcunha <davidgoncalvescunha@gmail.com>
2015-01-15 21:39:27 +0000

    Decorator pattern

--- decorator_pattern.rb
+++ decorator_pattern.rb
@@ -1,19 +1,77 @@
-#Decorator Pattern
+# Decorator Pattern
 
-module Decorator1
-  def do_something(common_item_to_decorate)
-   #code
+class SimpleWriter
+  def initialize(path)
+    @file = File.open(path, 'w')
+  end
+
+  def write_line(line)
+    @file.print(line)
+    @file.print("\n")
+  end
+
+  def pos
+    @file.pos
+  end
+
+  def rewind
+    @file.rewind
+  end
+
+  def close
+    @file.close
   end
 end
 
-module Decorator2
-  def do_something(common_item_to_decorate)
-   #code
+class WriterDecorator
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+
+  def write_line(line)
+    @real_writer.write_line(line)
+  end
+
+  def pos
+    @real_writer.pos
+  end
+
+  def rewind
+    @real_writer.rewind
+  end
+
+  def close
+    @real_writer.close
   end
 end
 
-d = SimpleItem.new()
-d.extend(Decorator1)
-d.extend(Decorator2)
+class NumberingWriter < WriterDecorator
+  def initialize(real_writer)
+    super(real_writer)
+    @line_number = 1
+  end
 
-e.do_something('howdy')
+  def write_line(line)
+    @real_writer.write_line("#{@line_number}: #{line}")
+    @line_number += 1
+  end
+end
+
+writer = NumberingWriter.new(SimpleWriter.new('final.txt'))
+writer.write_line('Hello out there')
+
+# Decorator Pattern with Forwardable
+
+require 'forwardable'
+class WriterDecorator
+  extend Forwardable
+
+  def_delegators :@real_writer, :write_line, :rewind, :pos, :close
+
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+end
+
+writer = WriterDecorator.new(SimpleWriter.new('final.txt'))
+writer.write_line('Hello out there')
