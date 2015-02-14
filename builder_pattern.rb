904b5d05e3dc518350d54d031b0e67151acf6d30
davidcunha <davidgoncalvescunha@gmail.com>
2015-02-14 16:24:21 +0000

    Builder pattern

---
+++ builder_pattern.rb
@@ -0,0 +1,87 @@
+# Builder Pattern
+class ComputerBuilder
+  attr_reader :computer
+  def initialize
+    @computer = Computer.new
+  end
+
+  def turbo(has_turbo_cpu=true)
+    @computer.motherboard.cpu = TurboCPU.new
+  end
+
+  def display=(display)
+    @computer.display=display
+  end
+
+  def memory_size=(size_in_mb)
+    @computer.motherboard.memory_size = size_in_mb
+  end
+
+  def add_cd(writer=false)
+    @computer.drives << Drive.new(:cd, 760, writer)
+  end
+
+  def add_dvd(writer=false)
+    @computer.drives << Drive.new(:dvd, 4000, writer)
+  end
+  def add_hard_disk(size_in_mb)
+    @computer.drives << Drive.new(:hard_disk, size_in_mb, true)
+  end
+end
+
+# Builder Pattern (polymorphic)
+class ComputerBuilder
+  attr_reader :computer
+
+  def turbo(has_turbo_cpu=true)
+    @computer.motherboard.cpu = TurboCPU.new
+  end
+
+  def memory_size=(size_in_mb)
+    @computer.motherboard.memory_size = size_in_mb
+  end
+end
+
+class DesktopBuilder < ComputerBuilder
+  def initialize
+    @computer = DesktopComputer.new
+  end
+
+  def display=(display)
+    @display = display
+  end
+
+  def add_cd(writer=false)
+    @computer.drives << Drive.new(:cd, 760, writer)
+  end
+
+  def add_dvd(writer=false)
+    @computer.drives << Drive.new(:dvd, 4000, writer)
+  end
+end
+
+class LaptopBuilder < ComputerBuilder
+  def initialize
+    @computer = LaptopComputer.new
+  end
+
+  def display=(display)
+    raise "Laptop display must be lcd" unless display == :lcd
+  end
+
+  def add_cd(writer=false)
+    @computer.drives << LaptopDrive.new(:cd, 760, writer)
+  end
+
+  def add_dvd(writer=false)
+    @computer.drives << LaptopDrive.new(:dvd, 4000, writer)
+  end
+
+  def add_hard_disk(size_in_mb)
+    @computer.drives << LaptopDrive.new(:hard_disk, size_in_mb, true)
+  end
+end
+
+  def add_hard_disk(size_in_mb)
+  @computer.drives << Drive.new(:hard_disk, size_in_mb, true)
+  end end
